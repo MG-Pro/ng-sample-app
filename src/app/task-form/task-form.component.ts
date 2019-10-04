c53101a22718eeec06c1d5100f89cd98a3e04624
MG <mg84@bk.ru>
2019-10-04 20:39:01 +0300

    improve

--- src/app/task-form/task-form.component.ts
+++ src/app/task-form/task-form.component.ts
@@ -6,27 +6,27 @@ import {Task} from '../app.component'
   templateUrl: './task-form.component.html',
   styleUrls: ['./task-form.component.scss']
 })
+
 export class TaskFormComponent {
   @Output() onAdd: EventEmitter<Task> = new EventEmitter<Task>()
 
   @ViewChild('counter', {static: false}) counterRef: ElementRef
 
-  id:number = 0
+  id: number = 0
   newTitle: string = ''
   newContent: string = ''
 
 
-
   add() {
-    if(!this.newTitle.trim() || !this.newContent.trim()) {
-      return;
+    if (!this.newTitle.trim() || !this.newContent.trim()) {
+      return
     }
     const task: Task = {
       title: this.newTitle,
       content: this.newContent,
       id: this.id++
     }
-    this.onAdd.emit(task);
-    console.log(this.counterRef);
+    this.onAdd.emit(task)
+    console.log(this.counterRef)
   }
 }
